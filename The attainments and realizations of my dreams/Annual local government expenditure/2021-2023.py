fde28d0d15f034641665128e8710c8e64ea70e34
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-08-02 21:42:04 +0800

    Committed on or around 2024/08/02

--- The attainments and realizations of my dreams/Annual local government expenditure/2021-2023.py
+++ The attainments and realizations of my dreams/Annual local government expenditure/2021-2023.py
@@ -3,9 +3,9 @@ import numpy as np
 
 Year = ("2021", "2023", "2024")
 penguin_means = {
-    'Taipei': (18.35, 177.48, 190.71),
+    'Taipei': (171.58, 177.48, 190.71),
     'New Taipei': (38.79, 197.57, 212.48),
-    'Kaohsiung': (189.95, 160.85, 169.57),
+    'Kaohsiung': (151.44, 160.85, 169.57),
 }
 
 x = np.arange(len(Year))  # the label locations
