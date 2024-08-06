8aea39d2e38a1c27132e5263e1b77b6b1cb216e3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2024-08-06 16:24:57 +0800

    Committed on or around 2024/08/06

--- The attainments and realizations of my dreams/Annual local government expenditure/2022-2024_at_metro_level.py
+++ The attainments and realizations of my dreams/Annual local government expenditure/2022-2024_at_metro_level.py
@@ -4,9 +4,12 @@ import numpy as np
 Year = ("2022", "2023", "2024")
 penguin_means = {
                      # Taipei + New Taipei + Keelung
-    'Greater Taipei': (171.58, 177.48+197.57+19.63, 190.71+212.48+24.09),
+    'Greater Taipei': (171.58+186.48+21.15, 177.48+197.57+19.63, 190.71+212.48+24.09),
                      # Taoyuan + Hsinchu county + Hsinchu city + Miaoli
-    'Taoyuan-Hsinchu_Miaoli': (141.05, 142.99+34.82+25.51+22.98, 156.80+35.43+28.11+25.05),
+    'Taoyuan-Hsinchu_Miaoli': (141.05+32.36+25.61+21.56,
+                               142.99+34.82+25.51+22.98,
+                               156.80+35.43+28.11+25.05),
+
     'Taichung-Changhua-Nantou': (151.20, 150.48+58.89+24.92, 177.22+64.45+33.72),
                      # Yunlin + Chiayi county + Chiayi city + Tainan
     'Yunlin-Chiayi-Tainan': (102.06, 36.30+27.41+18.38+102.56, 40.43+33.22+19.35+116.94),
