85401375440f63d2a2947d387b8a0dbb3938a57f
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2025-03-11 11:39:39 +0800

    Committed on or around 2025/03/11

--- The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/2025/February/At metro level color.py
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/2025/February/At metro level color.py
@@ -43,7 +43,7 @@ img = image.imread('CC0.png')  # CC0.png downloaded from [7].
 plt.figimage(X=img, xo=800, yo=800, alpha=0.9)
 
 # Insert text watermark [1]
-plt.text(x=0.6, y=0.7, s="CC0 Public Domain\n無版權圖片", fontsize=40, color='grey', alpha=0.9,
+plt.text(x=0.6, y=0.7, s="Non-copyrighted image\n無版權圖片", fontsize=40, color='grey', alpha=0.9,
          ha='center', va='center', rotation=30,
          transform=ax.transAxes) # data coordinates [2] [Note1] [3] [Note2]
 
