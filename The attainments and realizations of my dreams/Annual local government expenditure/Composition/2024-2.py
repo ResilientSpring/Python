a8cfc03c4c9db19e6d8e5e9383f3877b1bfc5ec5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2025-03-20 12:38:19 +0800

    Committed on or around 2025/03/20

---
+++ The attainments and realizations of my dreams/Annual local government expenditure/Composition/2024-2.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+species = ('Taipei', 'Chinstrap', 'Kaohsiung')
+sex_counts = {
+    'Statutory': np.array([73, 34, 61]),
+    'Basic subsidy': np.array([73, 34, 58]),
+}
+width = 0.6  # the width of the bars: can also be len(x) sequence
+
+
+fig, ax = plt.subplots()
+bottom = np.zeros(3)
+
+for sex, sex_count in sex_counts.items():
+    p = ax.bar(species, sex_count, width, label=sex, bottom=bottom)
+    bottom += sex_count
+
+    ax.bar_label(p, label_type='center')
+
+for rect in plot:
+    height = rect.get_height()
+    ax.text(rect.get_x() + rect.get_width() / 2., 1.002 * height,
+            '%d' % int(height), ha='center', va='bottom', fontsize=12)
+
+ax.set_title('Number of penguins by sex')
+ax.legend()
+
+plt.show()
