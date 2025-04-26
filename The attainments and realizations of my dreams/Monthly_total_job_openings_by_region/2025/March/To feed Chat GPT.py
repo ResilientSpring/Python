c4dd483251140b0fcafef70241bd5bf22ea06fb3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2025-04-26 13:56:02 +0800

    Committed on or around 2025/04/26

---
+++ The attainments and realizations of my dreams/Monthly_total_job_openings_by_region/2025/March/To feed Chat GPT.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.cbook as cbook
+import matplotlib.colors as colour
+import matplotlib.image as image
+# Using the magic encoding
+# -*- coding: utf-8 -*-
+from matplotlib.transforms import IdentityTransform
+
+matplotlib.rc('font', family="MS Gothic")
+
+region_num = [1]
+position_vacancies = [39865]
+
+label = ["高雄市\nKaohsiung City"]
+
+fig, ax = plt.subplots(figsize=(9, 9))
+plt.xticks(region_num, labels=label, rotation=7, fontsize=12)
+# plt.yticks([0, 250, 300, 350, 400, 450, 500, 550])
+plt.tick_params(axis='y', labelsize=12) # [2]
+
+plot = ax.bar(region_num, position_vacancies, edgecolor='black', color=[colour.CSS4_COLORS.get('mediumslateblue'),
+                                                                        colour.CSS4_COLORS.get('slateblue'),
+                                                                        colour.CSS4_COLORS.get('darkslateblue'),
+                                                                        colour.CSS4_COLORS.get('mediumpurple'),
+                                                                        colour.CSS4_COLORS.get('rebeccapurple')]) #[4][5][6]
+
+for rect in plot:
+    height = rect.get_height()
+    ax.text(rect.get_x() + rect.get_width() / 2., 1.002 * height,
+            '%d' % int(height), ha='center', va='bottom', fontsize=12)
+
+plt.title("2024/02 台灣南北二城市職缺數\n the number of job openings in Taipei & Kaohsiung of Taiwan", fontsize=20)
+
+# plt.ylabel("")
+plt.xlabel("參考資料 Reference https://archive.ph/LPccQ")
+
+plt.ylim(30000, 130000)
+
+plt.figimage(X=img, xo=600, yo=700, alpha=0.9)
+
+# Insert text watermark [1]
+plt.text(x=0.6, y=0.7, s="CC-BY 4.0", fontsize=40, color='grey', alpha=0.9,
+         ha='center', va='center', rotation=30,
+         transform=ax.transAxes) # data coordinates [2] [Note1] [3] [Note2]
+
+plt.show()
